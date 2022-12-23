c6002fd5a13ade5c49be03712a4060d2da7570ff
Mohsin Ali <mohsinalisep@gmail.com>
2022-12-23 23:37:18 +0500

    Added longest sub-array with k unique characters problem

---
+++ interview_prep/sliding_window/variable_size_sliding_window/2_longest_sub_array_with_k_unique_letters.py
@@ -0,0 +1,25 @@
+from collections import  Counter
+
+
+arr = [10, 20, 30, 40, 12, 10, 30, 100, 100, 100, 100, 100, 100]
+k = 3
+start, end = 0, 0
+result = 0
+buffer = []
+
+while end < len(arr):
+    buffer.append(arr[end])
+    unique_count = len(Counter(buffer)) 
+    if unique_count < k:
+        end += 1
+    elif unique_count == k:
+        result = max(result, len(buffer))
+        end += 1
+    elif unique_count > k:
+        while unique_count > k:
+            del buffer[start]
+            unique_count = len(Counter(buffer)) 
+            start += 1
+        end += 1
+
+pass
