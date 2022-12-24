6a8759af2728ee6d08cc762a84fb37e73e25a43e
Mohsin Ali <mohsinalisep@gmail.com>
2022-12-24 07:53:18 +0500

    Improved the calculations part

--- interview_prep/sliding_window/variable_size_sliding_window/2_longest_sub_array_with_k_unique_letters.py
+++ interview_prep/sliding_window/variable_size_sliding_window/2_longest_sub_array_with_k_unique_letters.py
@@ -5,20 +5,24 @@ arr = [10, 20, 30, 40, 12, 10, 30, 100, 100, 100, 100, 100, 100]
 k = 4
 start, end = 0, 0
 result = 0
-buffer = []
+buffer = Counter()
 
 while end < len(arr):
-    buffer.append(arr[end])
-    unique_count = len(Counter(buffer)) 
-    if unique_count < k:
+    buffer.update([arr[end]])
+    if len(buffer) < k:
         end += 1
-    elif unique_count == k:
-        result = max(result, len(buffer))
+    elif len(buffer) == k:
+        result = max(result, sum(buffer.values()))
         end += 1
-    elif unique_count > k:
-        while unique_count > k:
-            buffer.remove(arr[start])
-            unique_count = len(Counter(buffer)) 
+    elif len(buffer) > k:
+        while len(buffer) > k:
+            value = buffer[arr[start]]
+            
+            if (value - 1) == 0:
+                del buffer[arr[start]] 
+            else:
+                buffer.update({arr[start]: -1})
+            
             start += 1
         end += 1
 
